fffd893491e7fe4d51ebbc17a1ff157f904627c4
alexdev-kgd <alexpetrov248@gmail.com>
2022-05-27 15:35:27 +0200

    Some fallback statements

--- src/app/shared/choice-card/choice-card.component.ts
+++ src/app/shared/choice-card/choice-card.component.ts
@@ -15,21 +15,36 @@ export class ChoiceCardComponent implements OnInit {
 
   gotMatch: boolean = false;
 
+  isEmpty: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
     this.currentPerson = this.people[this.counter]
+    this.checkForEmptiness();
+  }
+
+  checkForEmptiness(): void {
+    if(!this.people.length) this.isEmpty = true;
   }
 
   like(): void {
     this.gotMatch = true;
   }
 
+  dislike(): void {
+    if (this.people.length === 1) this.isEmpty = true;
+    if (this.people.length > 1) this.people.splice(this.counter, 1);
+    if (!this.people[this.counter]) this.counter--;
+    this.currentPerson = this.people[this.counter]
+    this.checkForEmptiness();
+  }
+
   continue(): void {
     this.counter++;
     
-    if(this.gotMatch) this.gotMatch = false;
-    if(!this.people[this.counter]) this.counter = 0;
+    if (this.gotMatch) this.gotMatch = false;
+    if (!this.people[this.counter]) this.counter = 0;
 
     this.currentPerson = this.people[this.counter]
   }
